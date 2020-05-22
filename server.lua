83a68405c73e7ce230d956180e1d1bf95eac1204
fribblet56 <fribblet56@gmail.com>
2020-05-23 00:07:22 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -13,17 +13,22 @@ AddEvent("OnPlayerQuit", function(player)
 end)
 
 
-function SetWeapon(player, model, ammo, equipe, slot, bLoaded)
-    if player == nil or model == nil then print("[ERROR] Use this syntax SetWeapon(player, model, ammo, equipe, slot, bLoaded)") return end
+function SetWeapon(player, model, ammo, equip, slot, bLoaded)
+    if player == nil or model == nil then print("[ERROR] Use this syntax SetWeapon(player, model, ammo, equip, slot, bLoaded)") return end
     if slot == nil or slot > 4 or slot < 1 then slot = 1 end
     if ammo == nil or ammo < 0 then ammo = 1 end
     if equip == nil then equip = false end
     if bLoaded == nil then bLoaded = false end
 
-    UpdateWeaponPlayerList(player, model, ammo, equipe, slot, bLoaded)
-
-    SetPlayerWeapon(player, model, ammo, equip, slot, bLoaded)
-    CallRemoteEvent(player, "weapon:SetCheck", true)
+    getReponse = SetPlayerWeapon(player, model, ammo, equip, slot, bLoaded)
+    
+    if getReponse then
+        UpdateWeaponPlayerList(player, model, ammo, equip, slot, bLoaded)
+        CallRemoteEvent(player, "weapon:SetCheck", true)
+    else
+        print("[ERROR] SetWeapon invalid argument(s)")
+       return 
+    end
     
 end
 AddFunctionExport("SetWeapon", SetWeapon)
@@ -46,13 +51,13 @@ end
 AddRemoteEvent("weapon:VerifWeapon", VerifWeapon)
 
 function ReTrySetPlayerWeapon(player, slot)
-    SetPlayerWeapon(player, WeaponPlayer[player][slot].model, WeaponPlayer[player][slot].ammo, WeaponPlayer[player][slot].equipe, WeaponPlayer[player][slot].slot, WeaponPlayer[player][slot].bLoaded) 
+    SetPlayerWeapon(player, WeaponPlayer[player][slot].model, WeaponPlayer[player][slot].ammo, WeaponPlayer[player][slot].equip, WeaponPlayer[player][slot].slot, WeaponPlayer[player][slot].bLoaded) 
 end
 
-function UpdateWeaponPlayerList(player, model, ammo, equipe, slot, bLoaded)
+function UpdateWeaponPlayerList(player, model, ammo, equip, slot, bLoaded)
     WeaponPlayer[player][slot].model = model
     WeaponPlayer[player][slot].ammo = ammo
-    WeaponPlayer[player][slot].equipe = equipe
+    WeaponPlayer[player][slot].equip = equip
     WeaponPlayer[player][slot].slot = slot
     WeaponPlayer[player][slot].bLoaded = bLoaded
 end
